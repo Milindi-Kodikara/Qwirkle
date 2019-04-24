5ac25cf39608dac5b9e4a50c4311f5595d5a601b
Flynn Calcutt <flynn.calcutt@hotmail.com>
2019-04-24 18:53:11 +1000

    Added GameEngine class, filled in a little bit of the menu displaying funtionality

---
+++ GameEngine.h
@@ -0,0 +1,12 @@
+#ifndef ASSIGN2_GAMEENGINE_H
+#define ASSIGN2_GAMEENGINE_H
+
+#include "Tile.h"
+#include "LinkedList.h"
+
+class GameEngine
+{
+
+};
+
+#endif // ASSIGN2_GAMEENGINE_H
--- qwirkle.cpp
+++ qwirkle.cpp
@@ -1,15 +1,80 @@
-
-#include "LinkedList.h"
-
+#include "GameEngine.h"
 #include <iostream>
+#include <string>
+#include <exception>
 
 #define EXIT_SUCCESS    0
 
-int main(void) {
-   LinkedList* list = new LinkedList();
-   delete list;
+void new_game()
+{
+	//TODO
+}
 
-   std::cout << "TODO: Implement Qwirkle!" << std::endl;
+void load_game()
+{
+	//TODO
+}
 
-   return EXIT_SUCCESS;
+void show_student_info()
+{
+	//TODO
+}
+
+void display_menu()
+{
+	bool exit = false;
+	while (!exit)
+	{
+		std::cout << "Menu\n"
+			<< "----\n"
+			<< "1. New Game\n"
+			<< "2. Load Game\n"
+			<< "3. Show Student Information\n"
+			<< "4. Quit\n\n";
+		bool valid_input = false;
+		while (valid_input)
+		{
+			// This system for checking EOF won't work, find better solution
+			std::cout << "> ";
+			std::string input;
+			std::cin >> input;
+			try
+			{
+				int choice = std::stoi(input);
+				exit = true;
+				valid_input = true;
+				if (choice == 1) new_game();
+				else if (choice == 2) load_game();
+				else if (choice == 3)
+				{
+					// Allows the menu to be shown again after the info is displayed
+					exit = false;
+					show_student_info();
+				}
+				else
+				{
+					valid_input = false;
+					cout << "Invalid Input" << endl;
+				}
+			}
+			catch (std::exception e)
+			{
+				// Checks if Ctrl D (EOF) was entered
+				if (input[0] == EOF)
+				{
+					valid_input = true;
+					exit = true;
+				}
+				cout << "Invalid Input" << endl;
+			}
+		}
+	}
+}
+
+int main(void) 
+{
+	std::cout << "Welcome to Qwirkle!" << std::endl;
+	std::cout << "-------------------" << std::endl;
+	display_menu();
+	return EXIT_SUCCESS;
 }
